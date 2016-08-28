5084c3b2c6a1767f79fa454e05b98eb74f18f01d
Allan Davidson <allan_m_e_davidson@hotmail.com>
2016-08-28 18:30:32 +0100

    laravel turotials

---
+++ task/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!doctype html>
+<html lang="en">
+  <head>
+      <title>Simple Checklist for Single User</title>
+      <!--
+
+        CSS, JavaScript, Other resources
+
+      -->
+  </head>
+    <body>
+      <div class="container">
+        <nav class="navbar navbar-default">
+          <!-- navigation options -->
+        </nav>
+      </div>
+
+      @yield('content')
+
+    </body>
+</html>
